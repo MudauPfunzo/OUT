c59cd4a25ff64d240b64b25bc4d96c92ac3d5abb
pfunz <pfunz@PFUNZO-PC>
2017-08-09 17:21:10 +0200

    Added Unit Test for Project

---
+++ CsvFileSortingTest/CsvFileSortingTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CsvFileSortingTest
+{
+    [TestClass]
+    public class CsvFileSortingTest
+    {
+        string dataFilePath = @"resources\file\data.csv";
+        [TestMethod]
+        public void Test_SortFileByFrequency()
+        {
+            string fileSavedPath = @"resources\file\OrderedByFrequency.txt";
+
+            Assert.IsTrue(File.Exists(dataFilePath),"Data File does not exist");
+            CSVFileSort.FileProcessing.FileProcessor fp = new CSVFileSort.FileProcessing.FileProcessor(dataFilePath);
+            Assert.IsNotNull(fp);
+            fp.SortFileByFrequency();
+            Assert.IsTrue(File.Exists(fileSavedPath), "OrderedByFrequency File not created");
+        }
+        [TestMethod]
+        public void Test_SortFileByStreetNameAsc()
+        {
+
+            string fileSavedPath = @"resources\file\OrderedByStreetName.txt";
+            Assert.IsTrue(File.Exists(dataFilePath), "Data File does not exist");
+            CSVFileSort.FileProcessing.FileProcessor fp = new CSVFileSort.FileProcessing.FileProcessor(dataFilePath);
+            Assert.IsNotNull(fp);
+            fp.SortFileByStreetNameAsc();
+            Assert.IsTrue(File.Exists(fileSavedPath), "OrderedByStreetName File not Created");
+        }
+    }
+}
