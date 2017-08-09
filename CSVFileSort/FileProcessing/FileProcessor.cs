515cae67d8532d7e6ce2c53c3b19b8d43efc67b2
pfunz <pfunz@PFUNZO-PC>
2017-08-09 16:50:57 +0200

    Added Functionality to Order by Street name

--- CSVFileSort/FileProcessing/FileProcessor.cs
+++ CSVFileSort/FileProcessing/FileProcessor.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,8 +14,8 @@ namespace CSVFileSort.FileProcessing
         public string FilePath { get; set; }
         public string OrderBy { get; set; }
         public string Sort { get; set; }
-
-
+        private string[] recordsFromFileWithHearder = new string[] { };
+        private IEnumerable<string> recordsFromFile;
         private string ProjectDirectory = Path.GetDirectoryName(Directory.GetParent(Environment.CurrentDirectory).ToString());
 
         public FileProcessor() { }
@@ -24,37 +25,67 @@ namespace CSVFileSort.FileProcessing
             this.OrderBy = orderby;
             this.Sort = sort;
         }
+        private void ReadFile()
+        {
+            recordsFromFileWithHearder = File.ReadAllLines(FilePath);
+            recordsFromFile = recordsFromFileWithHearder.Skip(1); 
+        }
 
         public void SortFileByFrequency()
         {
-            string savePath = @"resources\file\test.txt";
-            string[] recordsFromFileWithHearder = new string[] { };
+            //Read the File
+            ReadFile();
 
-            recordsFromFileWithHearder = File.ReadAllLines(FilePath);
-            var recordsFromFile = recordsFromFileWithHearder.Skip(1);
-            var groupRecords = recordsFromFile
-                                            .Select(record => new
-                                            {
-                                                FirstName = record.Split(',')[0],
-                                                LastName = record.Split(',')[1]
 
-                                            })
-                                             .GroupBy(p => new
+            string savePath = @"resources\file\OrderedByFrequency.txt";
+            //Get the Firts Names from the file
+            var FirstNames = recordsFromFile.Select(record => new { Name = record.Split(',')[0] });
+            //Get the Last Names from the file
+            var LastNames = recordsFromFile.Select(record => new { Name = record.Split(',')[1] });
+
+            //Combine Last Names and First Names into one list
+            var FirstAndLastNames = FirstNames.Concat(LastNames);
+            //Group by the Name Field, and Count
+            var groupRecords = FirstAndLastNames
+                                            .GroupBy(p => new
+                                             {
+                                                 p.Name
+                                              
+                                            }).OrderBy(x=> x.Key.Name)
+                                            .Select(x => new
                                              {
 
-                                                 p.LastName
-                                             })
-                                              .OrderBy(x => x.Key.LastName)
-                                              .Select(x => new
-                                              {
-
-                                                  x.Key.LastName,
+                                                 // x.Key.LastName,
+                                                  x.Key.Name,
                                                   Frequency = x.Count()
-                                              });
-            List<string> records = groupRecords.Select(x => x.LastName + "," + x.Frequency.ToString()).ToList();
-
+                                             }).OrderByDescending(x => x.Frequency);
+           
+            //Get the Names and their count into a list 
+            List<string> records = groupRecords.Select(x => x.Name + "," + x.Frequency.ToString()).ToList();
+            //Write the names and count into a file
             File.WriteAllLines(Path.Combine(ProjectDirectory, savePath),records);
         }
-       
+        public void SortFileByStreetNameAsc()
+        {
+            //Read the File
+            ReadFile();
+
+
+            string savePath = @"resources\file\OrderedByStreetName.txt";
+            //Get Address from the file
+            var Address = recordsFromFile.Select(record => new {
+                StreetNumber = new string(record.Split(',')[2].TakeWhile(Char.IsDigit).ToArray()),
+                StreetName = Regex.Replace(record.Split(',')[2], @"[0-9\-]", string.Empty).Substring(1)
+                                                               }).OrderBy(x=>x.StreetName);
+
+          
+            
+
+            //Combine street number and street name
+            List<string> records = Address.Select(x => x.StreetNumber + " " + x.StreetName).ToList();
+            //Write the names and count into a file
+            File.WriteAllLines(Path.Combine(ProjectDirectory, savePath), records);
+        }
+
     }
 }
--- CSVFileSort/MainWindow.xaml.cs
+++ CSVFileSort/MainWindow.xaml.cs
@@ -21,16 +21,53 @@ namespace CSVFileSort
     /// </summary>
     public partial class MainWindow : Window
     {
+        FileProcessor fp = new FileProcessor(@"resources\file\data.csv", "", "");
         public MainWindow()
         {
             InitializeComponent();
-            orderFilByFrequenxy();
+           
         }
-        private void orderFilByFrequenxy()
+        private void orderFilByFrequency()
         {
-            FileProcessor fp = new FileProcessor(@"resources\file\data.csv","","");
+         
             fp.SortFileByFrequency();
             
+        }
+        private void OrderByStreetName()
+        {
+            fp.SortFileByStreetNameAsc();
+        }
+        private void EnableButtons()
+        {
+            cmdOpenFile1.IsEnabled = true;
+            cmdOpenFile2.IsEnabled = true;
+
+        }
+
+        private void cmdCreateFiles_Click(object sender, RoutedEventArgs e)
+        {
+            orderFilByFrequency();
+            OrderByStreetName();
+            EnableButtons();
+
+        }
+
+        private void cmdOpenFile1_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileInNotePad("OrderedByFrequency.txt");
+        }
+
+        private void cmdOpenFile2_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileInNotePad("OrderedByStreetName.txt");
+        }
+
+        private void OpenFileInNotePad(string fileName)
+        {
+            string ProjectPath = System.IO.Path.GetDirectoryName(System.IO.Directory.GetParent(Environment.CurrentDirectory).ToString());
+            string PathToFile = @"resources\file\" + fileName;
+            string fullPath = System.IO.Path.Combine(ProjectPath, PathToFile);
+            System.Diagnostics.Process.Start("notepad.exe", fullPath);
         }
     }
 }
